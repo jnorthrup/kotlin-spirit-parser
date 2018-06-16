68c61384fab614efd436e9d798da204a3d57902b
Jim Northrup <jim@vsiwest.com>
2018-06-16 09:32:48 +0700

    first whack at parser

---
+++ src/main/kotlin/com/github/jnorthrup/Token.kt
@@ -0,0 +1,5 @@
+package com.github.jnorthrup
+
+interface Token {
+
+}
--- src/test/kotlin/com/github/jnorthrup/HelloTest.kt
+++
@@ -1,8 +0,0 @@
-package com.github.jnorthrup
-
-import org.junit.Test
-import kotlin.test.assertEquals
-
-class HelloTest {
-
-}
