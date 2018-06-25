08d6c7c238ed169f132efdaf96d4722b98a1f160
Jim Northrup <jim@vsiwest.com>
2018-06-26 02:04:30 +0700

    nars grammar has a few kinks to iron out. disabling test.

--- src/test/kotlin/com/github/jnorthrup/narsese/NarseParserNal1Test.kt
+++ src/test/kotlin/com/github/jnorthrup/narsese/NarseParserNal1Test.kt
@@ -1,24 +1,58 @@
 package com.github.jnorthrup.narsese
 
-import com.github.jnorthrup.parser.overloads.get
-import com.github.jnorthrup.parser.overloads.invoke
-import com.github.jnorthrup.parser.overloads.plus
-import com.github.jnorthrup.parser.overloads.rem
+import com.github.jnorthrup.narsese.NarseParser.tokenize
+import com.github.jnorthrup.parser.overloads.*
 import com.github.jnorthrup.parser.primitives.Line
 import com.github.jnorthrup.parser.primitives.re
+import org.junit.Before
 import org.junit.Test
-
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class NarseParserNal1Test {
+    lateinit var input: Sequence<String>
+    @Before
+    fun setup() {
+
+        val resourceAsStream = NarseParserNal1Test::class.java.classLoader.getResourceAsStream("Examples/Example-NAL1-edited.txt")
+
+        val bufferedReader = BufferedReader(InputStreamReader(resourceAsStream))
+
+        input = generateSequence {
+            return@generateSequence bufferedReader.readLine()
+        }
+    }
+
+    @Test //java.lang.Exception: Method parseNal1() should be void
+    fun loadNal1() {
+
+        input.forEach {
+            print(it)
+
+        }
+
+    }
+
+    @Test //java.lang.Exception: Method parseNal1() should be void
+    fun parseNal1() {
+
+        val experiment = (re("\\*+"))[re(".*")..Unit]
+        val comment = ("//" / "OUT: ")[re(".*")..Unit]
+        val grammar = (experiment / comment / sentence / task)
+
+        input.toList().subList(0, 2).forEach {
+            grammar % tokenize(it)
+        }
+
+    }
+
     @Test
     fun testThen() = ("a"() + "b") % Line("a", "b")
 
     @Test
-    fun parseComment() = "//"[(re(".*"))] % Line("//", "comment", "here")
+    fun parseComment() = "//"[re(".*")..Unit] % Line("//", "comment", "here")
 
-//    @Test
-//    fun parseComment1() = ("//"() + seq(re(".*"))) % Line("//", "comment", "here")
+    @Test
+    fun parseComment1() = ("//" / "OUT: ")[re(".*")..Unit] % Line("//")
 
-//    @Test
-//    fun parseComment2() = ("//"[seq(re(".*"))]) % Line("//", "comment", "here")
 }
